d50ac8e16c2aaccd582d3dbf009212822ecf7c98
cory-coward <cory.coward@sac-inc.com>
2022-05-20 14:40:25 -0500

    Add fields to user login response dto

--- IntScience.API/Dtos/UserLoginResponseDto.cs
+++ IntScience.API/Dtos/UserLoginResponseDto.cs
@@ -5,6 +5,9 @@ public class UserLoginResponseDto
     public bool IsAuthenticationSuccessful { get; set; }
     public string ErrorMessage { get; set; }
     public string UserId { get; set; }
+    public string UserName { get; set; }
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
     public string Email { get; set; }
     public IList<string> Roles { get; set; }
     public string Token { get; set; }
